e32d2933fe472ee0545ae02c51d5286179f80952
Isaacodes-dev <mulengaisaac10@gmail.com>
2024-03-20 18:52:54 +0200

    added constants

---
+++ lib/constants/constants.dart
@@ -0,0 +1,7 @@
+const String baseUrl = 'http://192.168.0.109:8000/api';
+
+const String register = '/registeruser';
+
+const String login = '/loginuser';
+
+const String tizibaneUser = '/tizibaneuser';
