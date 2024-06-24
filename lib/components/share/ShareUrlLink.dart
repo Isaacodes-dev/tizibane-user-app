28f834b717c69de603d8e39e2ac92cf125569ce2
isaacodes-dadev <isaacmulenga12@outlook.com>
2024-06-24 15:37:12 +0200

    pull from main

--- lib/components/share/ShareUrlLink.dart
+++ lib/components/share/ShareUrlLink.dart
@@ -5,7 +5,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:tizibane/Services/UserService.dart';
 
 class ShareUrlLink extends StatelessWidget {
   const ShareUrlLink({super.key});
@@ -14,42 +13,19 @@ class ShareUrlLink extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
       onPressed: () async {
-        // Get the NRC from UserService
-        String? nrc = await _getUserNrc();
+        // Prepare vCard data
+        String vCardData = '''
+        BEGIN:VCARD
+        VERSION:3.0
+        FN:John Doe
+        TEL:+1234567890
+        ORG:Example Company
+        EMAIL:john.doe@example.com
+        END:VCARD
+        ''';
 
-        if (nrc != null) {
-          // Prepare vCard data
-          String vCardData = '''
-          BEGIN:VCARD
-          VERSION:3.0
-          FN:User
-          NOTE:$nrc
-          URL:https://tizibane.com/api/share/$nrc
-          END:VCARD
-          ''';
-
-          // Write the vCard data to a file and share it
-          await _shareVCard(vCardData);
-        }
-
-        // Show the popup when button is clicked
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: const Text('Share to Socials'),
-              content: Text('Share this vCard with others.'),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop(); // Close the dialog
-                  },
-                  child: const Text('Close'),
-                ),
-              ],
-            );
-          },
-        );
+        // Write the vCard data to a file and share it
+        await _shareVCard(context, vCardData);
       },
       backgroundColor: Colors.black, // Customize button color
       elevation: 4.0,
@@ -63,55 +39,42 @@ class ShareUrlLink extends StatelessWidget {
     );
   }
 
-  Future<String?> _getUserNrc() async {
-    UserService _userService = UserService();
-    await _userService.getUser();
-    String unformattedNrc = _userService.userObj.value[0].nrc;
-    return encryptNrc(unformattedNrc);
-  }
+  Future<void> _shareVCard(BuildContext context, String vCardData) async {
+    try {
+      // Get the directory to save the file.
+      final directory = await getApplicationDocumentsDirectory();
 
-  Future<String?> encryptNrc(String? nrc) async {
-    if (nrc == null) return null;
+      // Define the file path.
+      final filePath = '${directory.path}/contact.vcf';
 
-    int shift = 7;
-    String formattedNrc = '';
+      // Write the vCard data to the file.
+      final file = File(filePath);
+      await file.writeAsString(vCardData);
 
-    for (int i = 0; i < nrc.length; i++) {
-      int charCode = nrc.codeUnitAt(i);
-
-      if (charCode >= 97 && charCode <= 122) {
-        charCode -= 32;
-      }
-
-      if ((charCode >= 65 && charCode <= 90) ||
-          (charCode >= 48 && charCode <= 57)) {
-        int shiftedCharCode = (charCode + shift);
-
-        if (shiftedCharCode > 90 && charCode <= 90) {
-          shiftedCharCode = 65 + (shiftedCharCode - 91);
-        } else if (shiftedCharCode > 57) {
-          shiftedCharCode = 48 + (shiftedCharCode - 58);
-        }
-
-        formattedNrc += String.fromCharCode(shiftedCharCode);
-      }
+      // Share the file.
+      await Share.shareFiles([filePath], text: 'Here is a contact file.');
+    } catch (e) {
+      _showErrorDialog(context, 'Failed to share the vCard.');
     }
-
-    return formattedNrc;
   }
 
-  Future<void> _shareVCard(String vCardData) async {
-    // Get the directory to save the file.
-    final directory = await getApplicationDocumentsDirectory();
-
-    // Define the file path.
-    final filePath = '${directory.path}/contact.vcf';
-
-    // Write the vCard data to the file.
-    final file = File(filePath);
-    await file.writeAsString(vCardData);
-
-    // Share the file.
-    await Share.shareFiles([filePath], text: 'Here is a contact file.');
+  void _showErrorDialog(BuildContext context, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Error'),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
